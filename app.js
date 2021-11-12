4aae6bbfd55d765da95e1380b0845f5bad43b7eb
accubits-akshay <akshay.krishna@accubits.com>
2021-11-12 11:12:00 +0530

    logger middleware

--- app.js
+++ app.js
@@ -1,45 +1,48 @@
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const nodemailer =require("nodemailer");
-require('dotenv').config()
+const nodemailer = require("nodemailer");
+const morgan = require("morgan");
+require("./helpers/redis");
+
+require("dotenv").config();
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-require('./models/signup')
+require("./models/signup");
 
-app.use('/authentication',require('./routes/auth'))
+app.use(morgan("combined"));
 
+app.use("/authentication", require("./routes/auth"));
 
 //connect to DB
 mongoose.connect(
-    process.env.DB_URL,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    (err) => {
-      if (err) {
-        console.log(" err ", err);
-      } else console.log("Connected to DB");
-    }
-  );
-
+  process.env.DB_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      console.log(" err ", err);
+    } else console.log("Connected to DB");
+  }
+);
 
 //* Error Handler
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.json({
-      error: {
-          status: err.status || 500,
-          message: err.message
-      }
-  })
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
 });
 
-
 app.listen(process.env.PORT, (err) => {
-    if (err) {
-        console.log('Error ', err);
-    }
-    console.log('Node.js is running at PORT',process.env.PORT)})
+  if (err) {
+    console.log("Error ", err);
+  }
+  console.log("Node.js is running at PORT", process.env.PORT);
+});
